fa369de42aea33004905cd5b946163b253892fe8
Franck Panou <85650372+FPan7792@users.noreply.github.com>
2025-03-24 22:11:12 +0100

    feat: add features homepage section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
+import Features from './components/layouts/Features';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Features />
       <Footer />
     </>
   );
