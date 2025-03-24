7df1f92cd1997ee6951b5b607c07050724b97c6b
Nicolas <npierrefanfan@gmail.com>
2025-03-24 12:53:42 +0200

    feat: add nav menu from lib and create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-svh'>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Header />
+      <div className='flex flex-col items-center justify-center min-h-svh'>
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 }
 
