11fd4684ec8463877b1efb968f07017c2dba93d9
Nicolas <npierrefanfan@gmail.com>
2025-03-24 11:16:03 +0200

    chore: install prettier and setup lint-staged with husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className='flex flex-col items-center justify-center min-h-svh'>
       <Button>Click me</Button>
     </div>
   );
