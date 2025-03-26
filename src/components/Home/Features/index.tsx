15adcc18c781c7969dd6ee66afab69cb9e9d64cb
Nicolas <npierrefanfan@gmail.com>
2025-03-26 17:36:37 +0200

    feat(i18n): integrate react-i18next and translate messages

--- src/components/Home/Features/index.tsx
+++ src/components/Home/Features/index.tsx
@@ -1,18 +1,23 @@
 import { featuresDatas } from '@/ressources/datas/homepageMessages';
+import { useTranslation } from 'react-i18next';
 
-const Feature = () => (
-  <section className='container mx-auto w-full flex max-lg:flex-col max-lg:space-y-5 lg:space-x-7 pb-14'>
-    {featuresDatas?.map(({ title, icon: Icon, description }, idx) => (
-      <div
-        key={idx}
-        className=' shadow rounded flex-1 flex flex-col items-start px-5 py-5 '
-      >
-        <Icon strokeWidth={'3px'} className='text-blue-500' />
-        <p className='mt-1 font-bold'>{title}</p>
-        <p className='mt-3 text-sm'>{description}</p>
-      </div>
-    ))}
-  </section>
-);
+const Feature = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className='container mx-auto w-full flex max-lg:flex-col max-lg:space-y-5 lg:space-x-7 pb-14'>
+      {featuresDatas?.map(({ title, icon: Icon, description }, idx) => (
+        <div
+          key={idx}
+          className=' shadow rounded flex-1 flex flex-col items-start px-5 py-5 '
+        >
+          <Icon strokeWidth={'3px'} className='text-blue-500' />
+          <p className='mt-1 font-bold'>{t(title)}</p>
+          <p className='mt-3 text-sm'>{t(description)}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Feature;
---
+++ src/components/LanguageDropdown/index.tsx
@@ -0,0 +1,43 @@
+import { LANGUAGES } from '@/utils/i18n/constants';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentLanguage, setCurrentLanguage] = useState('🇬🇧');
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (code: string) => {
+    i18n.changeLanguage(code);
+    setCurrentLanguage(code === 'en' ? '🇬🇧' : '🇫🇷');
+    setIsOpen(false);
+  };
+
+  return (
+    <div className='p-2'>
+      <button
+        className='text-blue-60 cursor-pointer hover:text-accent-foreground'
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label={t('ariaLanguage')}
+      >
+        {currentLanguage}
+      </button>
+
+      {isOpen && (
+        <div className='absolute right-0 mt-2 w-30 bg-white border border-gray-300 shadow-lg rounded-md'>
+          {LANGUAGES.map((language) => (
+            <button
+              key={`${language.code}`}
+              className='block px-4 py-2 w-full text-left cursor-pointer hover:bg-gray-100 hover:text-accent-foreground'
+              onClick={() => changeLanguage(language.code)}
+            >
+              {language.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LanguageDropdown;
