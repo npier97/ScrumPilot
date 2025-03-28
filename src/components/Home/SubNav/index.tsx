52478bc11766cf863e9dba2ee75a327b61800b5f
Nicolas <npierrefanfan@gmail.com>
2025-03-28 23:38:42 +0200

    feat: add join room navigation

--- src/components/Home/SubNav/index.tsx
+++ src/components/Home/SubNav/index.tsx
@@ -4,13 +4,19 @@ import { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { useTranslation } from 'react-i18next';
 import { useCreateRoom } from '@/hooks/useCreateRoom';
+import { useNavigate } from '@tanstack/react-router';
 
 const SubNav = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [roomNumber, setRoomNumber] = useState('');
+
   const { createNewRoom } = useCreateRoom();
 
-  const handleClick = () => createNewRoom();
+  const handleCreateNewRoom = () => createNewRoom();
+  const handleJoinRoom = () =>
+    roomNumber && navigate({ to: `/join/${roomNumber}` });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,15 +34,22 @@ const SubNav = () => {
       } duration-300 ease-in-out`}
     >
       <div className='flex gap-2'>
-        <Input type='text' placeholder={t('subNavMessage1')} />
-        <Button className='bg-blue-600 cursor-pointer hover:bg-blue-500 md:max-w-[300px]'>
+        <Input
+          type='text'
+          placeholder={t('subNavMessage1')}
+          onChange={(e) => setRoomNumber(e.target.value)}
+        />
+        <Button
+          className='bg-blue-600 cursor-pointer hover:bg-blue-500 md:max-w-[300px]'
+          onClick={handleJoinRoom}
+        >
           {t('subNavMessage2')}
           <Merge aria-hidden='true' />
         </Button>
       </div>
       <Button
         className='bg-blue-600 cursor-pointer hover:bg-blue-500 md:max-w-[300px]'
-        onClick={handleClick}
+        onClick={handleCreateNewRoom}
       >
         {t('heroCTAMessage')}
         <Plus aria-hidden='true' />
